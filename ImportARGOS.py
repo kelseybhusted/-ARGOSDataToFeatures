a2b40131310ba2fc4c7e92bdc9a6d26fca4e9f52
kelseybhusted <kelsey.husted@duke.edu>
2022-10-19 14:34:29 -0400

    Add spatial reference to output

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -13,9 +13,19 @@
 # Import modules
 import sys, os, arcpy
 
+#Allow outputs to be overwritten
+arcpy.env.overwriteOutput = True
+
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+#Create feature class to which we will add feature
+outPath, outFile = os.path.split(outputFC)
+outputSR = arcpy.SpatialReference(53002)
+arcpy.management.CreateFeatureclass(outPath, outFile, "POINT", "", "", "", outputSR)
+
+
 
 #%% Construct a while loop and iterate through all lines in the data file
 # Open the ARGOS data file
